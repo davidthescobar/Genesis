8db6905f07454d4f2a5caadfe196489a44036728
davidthescobar <davidthescobar@outlook.com>
2023-06-19 23:23:07 -0500

    House-keeping changes from terminal

--- MachineLearning/machinelearning101.py
+++
@@ -1,7 +0,0 @@
-# Basic machine learning steps:
-# 1 - import the data
-# 2 - clean the data
-# 3 - split the data (training set, test set)
-# 4 - create a model (import algorithym)
-# 5 - check the output
-# 6 - improve
