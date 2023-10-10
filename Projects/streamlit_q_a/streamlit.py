98004fc002bbc9c202f17c65d03eb5203bbd0920
David Escobar <97133130+davidthescobar@users.noreply.github.com>
2023-10-09 21:33:46 -0500

    Add files via upload

---
+++ Projects/streamlit_q_a/streamlit.py
@@ -0,0 +1,149 @@
+import streamlit as st
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+import os
+
+
+def load_document(file):
+    import os
+    name, extension = os.path.splitext(file)
+    
+    if extension == '.pdf':
+        from langchain.document_loaders import PyPDFLoader
+        print(f'Loading {name}...')
+        loader = PyPDFLoader(file)
+    elif extension == '.docx':
+        from langchain.document_loaders import Docx2txtLoader
+        print(f'Loading {name}...')
+        loader = Docx2txtLoader(file)
+    elif extension == '.txt':
+        from langchain.document_loaders import TextLoader
+        print(f'Loading {name} ...')
+        loader = TextLoader(file)
+    elif extension == '.csv':
+        from langchain.document_loaders.csv_loader import CSVLoader
+        import sys
+        import csv
+        maxInt = sys.maxsize
+
+        while True:
+            # decrease the maxInt value by factor 10
+            # as long as the OverflowError occurs.
+
+            try:
+                csv.field_size_limit(maxInt)
+                break
+            except OverflowError:
+                maxInt = int(maxInt / 10)
+        print(f'Loading {name} ...')
+        loader = CSVLoader(file)
+    else:
+        print('Document format is not supported.')
+        return None
+        
+    data = loader.load()
+    return data
+
+
+# wikipedia data
+def load_from_wikipedia(query, lang='en', load_max_docs=2):
+    from langchain.document_loaders import WikipediaLoader
+    print(f'Loading {query}...')
+    loader = WikipediaLoader(query=query, lang=lang, load_max_docs=load_max_docs)
+    data = loader.load()
+    return data
+
+
+def chunk_data(data, chunk_size=256, chunk_overlap=20):
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    chunks = text_splitter.split_documents(data)
+    return chunks
+
+
+def create_embeddings(chunks):
+    embeddings = OpenAIEmbeddings()
+    vector_store = Chroma.from_documents(chunks, embeddings)
+    return vector_store
+
+
+# Higher 'k' value costs more money because it increases tokens used
+def get_answer(vector_store, q, k=3):
+    from langchain.chains import RetrievalQA
+    from langchain.chat_models import ChatOpenAI
+
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.2)
+
+    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': k})
+
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever)
+
+    answer = chain.run(q)
+    return answer
+
+
+def calculate_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-ada-002')
+    total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
+    # print(f'Total tokens: {total_tokens}.')
+    # print(f'Embedding cost in USD: {total_tokens / 1000 * 0.0004:.6f}.')
+    return total_tokens, total_tokens / 1000 * 0.0004
+
+
+def clear_history():
+    if 'history' in st.session_state:
+        del st.session_state['history']
+
+
+if __name__ == '__main__':
+     import os
+     from dotenv import load_dotenv, find_dotenv
+     load_dotenv(find_dotenv(), override=True)
+
+     st.subheader('LLM Question Answering App!')
+     with st.sidebar:
+         api_key = st.text_input('OpenAI API Key: ', type='password')
+         if api_key:
+             os.environ['OPENAI_API_KEY'] = api_key
+
+         uploaded_file = st.file_uploader('Upload File', type=['pdf', 'docx', 'txt', 'csv'])
+         chunk_size = st.number_input('Chunk size:', min_value=100, max_value=2048, value=512, on_change=clear_history)
+         k = st.number_input('k', min_value=1, max_value=10, value=3, on_change=clear_history)
+         add_data = st.button('Add Data', on_click=clear_history)
+
+         if uploaded_file and add_data:
+             with st.spinner('Reading, chunking, and embedding file ...'):
+                 bytes_data = uploaded_file.read()
+                 file_name = os.path.join('./', uploaded_file.name)
+                 with open(file_name, 'wb') as f:
+                     f.write(bytes_data)
+
+                 data = load_document(file_name)
+                 chunks = chunk_data(data, chunk_size=chunk_size)
+                 st.write(f'Chunk size: {chunk_size}, Chunks: {len(chunks)}')
+
+                 tokens, embedding_cost = calculate_embedding_cost(chunks)
+                 st.write(f'Embedding cost: ${embedding_cost:.4f}')
+
+                 vector_store = create_embeddings(chunks)
+
+                 st.session_state.vs = vector_store
+
+                 st.success('File Uploaded, chunked, and embedded successfully!')
+
+     q = st.text_input('Ask a question about the content of your file: ')
+     if q:
+         if 'vs' in st.session_state:
+             vector_store = st.session_state.vs
+             st.write(f'k: {k}')
+             answer = get_answer(vector_store, q, k=k)
+             st.text_area('LLM Answer: ', value=answer)
+
+         st.divider()
+         if 'history'not in st.session_state:
+             st.session_state.history = ''
+         value = f'Q: {q} \nA: {answer}'
+         st.session_state.history = f'{value} \n {"-" * 100} \n {st.session_state.history}'
+         h = st.session_state.history
+         st.text_area(label='Chat History', value=h, key='history', height=400)
